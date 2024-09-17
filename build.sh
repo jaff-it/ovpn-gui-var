d6ecefb1a20fcae3e2e2ef8e5288854abb6484e3
jaff-it <sergsayfulin@gmail.com>
2024-09-17 12:00:06 +0300

    refactor: change repos

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 sudo docker-compose -f docker-compose-w-mysql.yml build --no-cache
 
-aws ecr get-login-password --region eu-west-2 | sudo docker login --username AWS --password-stdin 373633196736.dkr.ecr.eu-west-2.amazonaws.com
+aws ecr get-login-password --region us-west-2 | sudo docker login --username AWS --password-stdin 956449821269.dkr.ecr.us-west-2.amazonaws.com
 
-sudo docker push 373633196736.dkr.ecr.eu-west-2.amazonaws.com/openvpn-server:latest
-sudo docker push 373633196736.dkr.ecr.eu-west-2.amazonaws.com/openvpn-webui:latest
-sudo docker push 373633196736.dkr.ecr.eu-west-2.amazonaws.com/openvpn-mysql:latest
+sudo docker push 956449821269.dkr.ecr.us-west-2.amazonaws.com/openvpn-server:latest
+sudo docker push 956449821269.dkr.ecr.us-west-2.amazonaws.com/openvpn-webui:latest
+sudo docker push 956449821269.dkr.ecr.us-west-2.amazonaws.com/openvpn-mysql:latest
